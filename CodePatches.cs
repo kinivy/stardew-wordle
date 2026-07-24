1fdd1b02c4334ee84911d61f19129df36eb0a6cd
brandon kinnick <77947023+kinivy@users.noreply.github.com>
2026-07-24 16:36:20 -0400

    Machine animation

--- CodePatches.cs
+++ CodePatches.cs
@@ -1,6 +1,8 @@
+using System.Xml;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Netcode;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.TerrainFeatures;
@@ -13,6 +15,7 @@ namespace StardewWordle
         static IMonitor Monitor { get; set; }
         static IModHelper Helper { get; set; }
         static string machineName = "kinivy_Wordle_WordleMachine";
+        static TimeSpan machineAnimInterval = TimeSpan.FromMilliseconds(1000);
 
         public static void Initialize(IMonitor monitor, IModHelper helper, Harmony harmony)
         {
@@ -26,12 +29,29 @@ namespace StardewWordle
                 original: AccessTools.Method(typeof(StardewValley.Object), nameof(StardewValley.Object.checkForAction)),
                 postfix: new HarmonyMethod(typeof(CodePatches), nameof(CheckForAction_Postfix))
             );
+			harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.Object), nameof(StardewValley.Object.updateWhenCurrentLocation)),
+                postfix: new HarmonyMethod(typeof(CodePatches), nameof(UpdateWhenLocation_Postfix))
+            );
         }
 		
 		static void Instance_Update_Postfix(GameTime gameTime) {
 			if(Game1.activeClickableMenu != null && Game1.activeClickableMenu.GetType().Name == "TestMenu")
             {
                 ((TestMenu) Game1.activeClickableMenu).update(gameTime);
+            }
+		}
+
+        static void UpdateWhenLocation_Postfix(StardewValley.Object __instance, GameTime time)
+		{
+			if(__instance.itemId.Contains(machineName))
+            {  
+                long second = (long)(time.TotalGameTime.TotalMilliseconds / 1000);
+                int idx = (int) (second % 2);
+                if(__instance.ParentSheetIndex != idx)
+                {
+			        __instance.ParentSheetIndex = idx;
+                }
             }
 		}
 
@@ -44,7 +64,6 @@ namespace StardewWordle
                     return;
                 }
                 Game1.activeClickableMenu = new TestMenu(Helper, Monitor);
-                return;
             }
         }
     }
